517a25ef87b854bc8cb024426893689c931f2017
llb0824-bb <480316883@qq.com>
2025-06-09 22:40:30 +0800

    Update rongguang.js

--- rongguang.js
+++ rongguang.js
@@ -1,12 +1,13 @@
-var body = $response.body;
-//我们就没有进行请求链接匹配了
-var obj = JSON.parse(body);
-//修改
- obj = JSON.parse(body);
- obj = JSON.parse($response.body);
-obj.data.ITEM_ID = "yubi10006";
-body = JSON.stringify(obj);
-$done({body});
+let obj = JSON.parse($response.body);
+let command = JSON.parse(obj.body.command); // 解析嵌套的 command 字符串
+
+// 替换 ProductID 的值
+command.Data.ProductID = 'consumable14';
+
+// 将修改后的 command 对象转回字符串
+obj.body.command = JSON.stringify(command);
+
+$done({body: JSON.stringify(obj)});
 /*
 参数说明
 title: 主标题
@@ -14,6 +15,4 @@ subtitle: 副标题
 body: 消息正文
 */
 
-$notification.post("Storm Sniffer","Network Debug Tool","最后的荣光充值修改成功!!!")
-
-
+$notification.post("Storm Sniffer","需要修改支付脚本请联系tg:@Kb0015","掌阅修改支付成功请查看!!!")
