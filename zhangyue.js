9b4f5f6a65c751a42bd2aa113d3fa7ce9412116e
llb0824-bb <480316883@qq.com>
2025-06-10 03:04:10 +0800

    Update zhangyue.js

--- zhangyue.js
+++ zhangyue.js
@@ -1,7 +1,22 @@
-let body = $response.body;
+/*******************************
+  本脚本老登制作
+需要定制破解支付内购联系
+tg@Kb0015
+*******************************
+[rewrite_local]
+^http[s]?:\/\/ih2.ireader.com\/trade\/market\/vip\/ios\/pay_agreement.+$ url script-response-body ireader.js
+[mitm] 
+hostname = ih2.ireader.com
+*******************************
+Surge
 
-// 使用正则表达式修改响应体中的ProductID
-body = body.replace(/"ProductID\\":\\".*?"/, '"ProductID\\":\\"consumable14\\"');
+[Script]
+^http[s]?:\/\/ih2.ireader.com\/trade\/market\/vip\/ios\/pay_agreement.+$ requires-body=1,max-size=0,script-path=ireader.js
 
-// 返回修改后的响应体
-$done({ body });
+[MITM]
+hostname = ih2.ireader.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/"ProductID\\\":\\".*?"/g,'"ProductID\\":\\"consumable14\\"');
+$done(body);
