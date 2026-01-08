7e6346632bb49e7323014e42d9215f87377bbdb8
llb0824-bb <480316883@qq.com>
2026-01-08 20:59:54 +0800

    Update islide.js

--- islide.js
+++ islide.js
@@ -25,5 +25,5 @@ hostname = store-api.islide.cc
 
 *******************************/
 var body=$response.body;
-body = body.replace(/"id\":3/g,'"id":23');
+body = body.replace(/"id\":\d+/g,'"id":23');
 $done(body);
