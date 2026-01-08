3e608ae129be30124b41c46d9136694e7abb5914
llb0824-bb <480316883@qq.com>
2026-01-08 21:25:10 +0800

    Update islide.js

--- islide.js
+++ islide.js
@@ -1,11 +1,11 @@
 /*******************************
-  公众号:木木IOS分享
+  老登出品
 关注了解更多新科技！！！
 islide
-脚本名称:islide
+脚本名称:islide会员
 使用声明：️此脚本仅供学习与交流，
         请勿转载与贩卖！️️️
-群1077223830
+定制脚本联系老登
 *******************************
 [rewrite_local]
 ^https?:\/\/store-api\.islide\.cc\/pay\/products\/listActive.*? url script-response-body https://raw.githubusercontent.com/llb0824-bb/storm/refs/heads/main/islide.js
@@ -30,7 +30,7 @@ try {
     if (obj.data && Array.isArray(obj.data)) {
         obj.data.forEach(item => {
             if (item.id === 3) {
-                item.id = 23; // 只修改 id=3 的项
+                item.id = 23; 
             }
         });
     }
