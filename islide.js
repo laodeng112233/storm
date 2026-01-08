b3ede7961185bec3b0be6e053d4d2a811fdccb70
llb0824-bb <480316883@qq.com>
2026-01-08 19:52:01 +0800

    Create islide.js

---
+++ islide.js
@@ -0,0 +1,27 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+islide
+脚本名称:islide
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/store-api.islide.cc\/pay\/products\/listActive.+$ url script-response-body islide.js
+[mitm] 
+hostname = store-api.islide.cc
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/store-api.islide.cc\/pay\/products\/listActive.+$ requires-body=1,max-size=0,script-path=islide.js
+
+[MITM]
+hostname = store-api.islide.cc
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data.id= 23;
+    $done({body: JSON.stringify(obj)});
+
